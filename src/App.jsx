659ea7232a6bcfce4c2900316059c07d412cfea1
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-08-25 16:15:33 +0200

    Add About route.

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ export default function App() {
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
