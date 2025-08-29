8be05610cb31a8d47990674963e3fa6949655210
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-08-29 18:14:37 +0200

    Add HostVanDetail shared UI.

---
+++ src/pages/Host/HostVanInfo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HostVanInfo() {
+  return <h1>Info page goes in here!</h1>;
+}
---
+++ src/pages/Host/HostVanPricing.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HostVanPricing() {
+  return <h1>Pricing page goes in here!</h1>;
+}
